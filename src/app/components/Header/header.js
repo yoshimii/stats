d1c5aff75b682b64aed85c9b0aa254099066e958
yoshimii <zarate.emm@gmail.com>
2024-09-18 20:16:27 -0500

    fix: top nav image

--- src/app/components/Header/header.js
+++ src/app/components/Header/header.js
@@ -14,7 +14,7 @@ const Header = () => {
                 </div>
             </nav>
             <div className="flex flex-col relative text-white">
-            <Image alt="banner image of skater helmets with a red filter overlay" src="/top.png" layout="responsive" width="234" height="43" className="absolute"/>
+            <Image alt="banner image of skater helmets with a red filter overlay" src="/top.png" layout="responsive" width="234" height="43" className="absolute z-40"/>
             <Image alt="banner image of skater helmets with a red filter overlay" src="/helmets.jpg" layout="responsive" width="1400" height="43" className="relative"/>
             <Image alt="banner image of skater helmets with a red filter overlay" src="/bottom.png" layout="responsive" width="234" height="43" className="absolute -bottom-px"/>
             <p className={`absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ${permanentMarker.className} text-9xl`}>Statistics</p>
