b9c2e27f5a0714a63be3fa7a4966aaed726d210d
yoshimii <zarate.emm@gmail.com>
2024-09-18 20:17:18 -0500

    feat: calculate season stats by skater id

--- src/app/components/Table/data/skaterDetails.js
+++ src/app/components/Table/data/skaterDetails.js
@@ -11,56 +11,67 @@ import Salty from '../SkaterPhotos/Salty.jpg'
 import Slam from '../SkaterPhotos/Slam.jpg'
 
 const allSkaters = [{
+    skaterId: 1,
     skaterName: 'Aggrobat',
     skaterNumber: '343m/s',
     team: 'Hellcats',
     photo: Aggro
 },{
+    skaterId: 2,
     skaterName: 'Ambitchous',
     skaterNumber: '8055',
     team: 'Hellcats',
     photo: Ambi
 },{
+    skaterId: 3,
     skaterName: 'Anya Marx',
     skaterNumber: '123',
     team: 'Cherry Bombs',
     photo: Anya
 },{
+    skaterId: 4,
     skaterName: 'Assault & Vinegar',
     skaterNumber: 'NaCl',
     team: 'Holy Rollers',
     photo: Salty
 },{
+    skaterId: 5,
     skaterName: 'Bad Habbitch',
     skaterNumber: 'fml',
     team: 'Las Putas del Fuego',
     photo: Habbitch
 },{
+    skaterId: 6,
     skaterName: 'Beaux Dacious',
     skaterNumber: '21',
     team: 'Rhinestones',
     photo: Beaux
 },{
+    skaterId: 7,
     skaterName: 'Beetle Jukes',
     skaterNumber: '3 T1MES',
     team: 'Hired Gun$',
     photo: Bj
 },{
+    skaterId: 8,
     skaterName: 'Boogie Heights',
     skaterNumber: '77',
     team: 'Cherry Bombs',
     photo: Boogie
 },{
+    skaterId: 9,
     skaterName: 'Cannabish',
     skaterNumber: 'W33D',
     team: 'Rhinestones',
     photo: Bish
 },{
+    skaterId: 10,
     skaterName: 'Colonel Slamders',
     skaterNumber: '3AT M3',
     team: 'Rhinestones',
     photo: Slam
 },{
+    skaterId: 11,
     skaterName: 'Cyd Vicious',
     skaterNumber: '1113',
     team: 'Hellcats',
--- src/app/components/Table/table.js
+++ src/app/components/Table/table.js
@@ -1,62 +1,92 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import Row from "./row";
 import skaterDetails from "./data/skaterDetails";
 import stats from "./data/allSkaterStats";
 
 const Table = () => {
+    const [sortBy, setSortBy] = useState('')
     const scoredWhileInPlay = stats.reduce((n, {scoredWhileInPlay}) => n + scoredWhileInPlay, 0)
     const pointsAllowed = stats.reduce((n, {pointsAllowed}) => n + pointsAllowed, 0)
     const pointsLost = stats.reduce((n, {pointsLost}) => n + pointsLost, 0)
 
-    const ambiStats = {
-        gamesPlayed: stats.length,
-        scoredWhileInPlay,
-        assists: stats.reduce((n, {assists}) => n + assists, 0),
-        overallPoints: scoredWhileInPlay - pointsAllowed - pointsLost,
-        avgPointsPerJam: (stats.reduce((n, {ptsPerJam}) => n + ptsPerJam, 0)/stats.length).toFixed(3),
-        jamCount: stats.reduce((n, {jamCount}) => n + jamCount, 0),
-        pointsAllowed,
-        pointsLost,
+    const handleChange = (e) => {
+        setSortBy(e.target.value)
     }
-    const allSkaterStats = skaterDetails.map((skaterInfo) => {
+    // Group stats by skater Id
+    const groupedSkaterStats = stats.reduce((skater, stat) => {
+        (skater[stat.skaterId] = skater[stat.skaterId] || []).push(stat);
+        return skater;
+
+    }, {});
+
+    // Build skater stats for a season (year)
+    const allSkaterSeasonStats = skaterDetails.map(({skaterId, skaterName, skaterNumber, team, photo}) => {
+        const scoredWhileInPlay = groupedSkaterStats[skaterId].reduce((n, {scoredWhileInPlay}) => n + scoredWhileInPlay, 0)
+        const pointsAllowed = groupedSkaterStats[skaterId].reduce((n, {pointsAllowed}) => n + pointsAllowed, 0)
+        const pointsLost = groupedSkaterStats[skaterId].reduce((n, {pointsLost}) => n + pointsLost, 0)
+        const overallPoints = scoredWhileInPlay - pointsAllowed - pointsLost
         return {
-            ...skaterInfo,
-            ...ambiStats
+            skaterId,
+            skaterName,
+            skaterNumber,
+            team,
+            photo,
+            season: groupedSkaterStats[skaterId][0].season,
+            scoredWhileInPlay,
+            assists: groupedSkaterStats[skaterId].reduce((n, {assists}) => n + assists, 0),
+            overallPoints,
+            averagePointsPerJam: groupedSkaterStats[skaterId].reduce((n, {pointsPerJam}) => n + pointsPerJam, 0)/groupedSkaterStats[skaterId].length,
+            jamCount: groupedSkaterStats[skaterId].reduce((n, {jamCount}) => n + jamCount, 0),
+            pointsAllowed,
+            pointsLost
         }
     })
+
     return (
-<div className="flex justify-center">
-    <table className="text-left	">
-        <tbody>
-        <tr>
-            <th className="pr-4">Name</th>
-            <th className="pr-4">Number</th>
-            <th className="pr-4">Team</th>
-            {/* <th className="pr-4">Position</th> */}
-            <th className="pr-4">GP</th>
-            <th className="pr-4">SWIP</th>
-            <th className="pr-4">A</th>
-            <th className="pr-4">OP</th>
-            <th className="pr-4">APJ</th>
-            <th className="pr-4">JC</th>
-            <th className="pr-4">PA</th>
-            <th className="pr-4">PL</th>
-        </tr>
-        {allSkaterStats.map((s) => {
-            return <Row key={s.skaterName} photo={s.photo} name={s.skaterName} team={s.team} number={s.skaterNumber} 
-            gamesPlayed={s.gamesPlayed} 
-            scoredWhileInPlay={s.scoredWhileInPlay} 
-            assists={s.assists}
-            overallPoints={s.overallPoints}
-            avgPointsPerJam={s.avgPointsPerJam}
-            jamCount={s.jamCount}
-            pointsAllowed={s.pointsAllowed}
-            pointsLost={s.pointsLost}
-            />
-        })}
-        </tbody>
-    </table>
-</div>
+    <div className="flex flex-col items-center">
+        <div className="flex justify-start max-w-5xl min-w-60 w-min">
+            <select onChange={handleChange}>
+                <option value=''>All Teams</option>
+                <option value='cherryBombs'>Cherry Bombs</option>
+                <option value='hellcats'>Hellcats</option>
+                <option value='hiredGuns'>Hired Guns</option>
+                <option value='holyRollers'>Holy Rollers</option>
+                <option value='lasPutasDelFuego'>Las Putas del Fuego</option>
+                <option value='rhinestones'>Rhinestones</option>
+            </select>
+        </div>
+        <table className="text-left">
+            <tbody>
+            <tr>
+                <th className="pr-4">Name</th>
+                <th className="pr-4">Number</th>
+                <th className="pr-4">Team</th>
+                {/* <th className="pr-4">Position</th> */}
+                <th className="pr-4">GP</th>
+                <th className="pr-4">SWIP</th>
+                <th className="pr-4">A</th>
+                <th className="pr-4">OP</th>
+                <th className="pr-4">APJ</th>
+                <th className="pr-4">JC</th>
+                <th className="pr-4">PA</th>
+                <th className="pr-4">PL</th>
+            </tr>
+            {allSkaterSeasonStats.map((s) => {
+                return <Row key={s.skaterName} photo={s.photo} name={s.skaterName} team={s.team} number={s.skaterNumber} 
+                gamesPlayed={s.gamesPlayed} 
+                scoredWhileInPlay={s.scoredWhileInPlay} 
+                assists={s.assists}
+                overallPoints={s.overallPoints}
+                avgPointsPerJam={s.averagegPointsPerJam}
+                jamCount={s.jamCount}
+                pointsAllowed={s.pointsAllowed}
+                pointsLost={s.pointsLost}
+                />
+            })}
+            </tbody>
+        </table>
+    </div>
     )
 }
 
