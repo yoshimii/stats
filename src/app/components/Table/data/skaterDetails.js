6196e81bb0a62650ce0e2abce01716b761d8ce8f
yoshimii <zarate.emm@gmail.com>
2024-10-05 01:34:00 -0500

    feat: holy roller skater details

--- src/app/components/Table/data/skaterDetails.js
+++ src/app/components/Table/data/skaterDetails.js
@@ -1,14 +1,28 @@
 import Aggro from '../SkaterPhotos/Aggro.jpg'
+import Al from '../SkaterPhotos/Feared.jpg'
 import Ambi from '../SkaterPhotos/Ambi.jpg'
 import Anya from '../SkaterPhotos/Anya.jpg'
 import Beaux from '../SkaterPhotos/Beaux.jpg'
-import Bish from '../SkaterPhotos/Bish.jpg'
-import Bj from '../SkaterPhotos/Bj.jpg'
+import Bish from '../SkaterPhotos/Cannabish.jpg'
+import Bj from '../SkaterPhotos/Beetlejukes.jpg'
 import Boogie from '../SkaterPhotos/Boogie.jpg'
 import Cyd from '../SkaterPhotos/Cyd.jpg'
-import Habbitch from '../SkaterPhotos/Habbitch.jpg'
-import Salty from '../SkaterPhotos/Salty.jpg'
-import Slam from '../SkaterPhotos/Slam.jpg'
+import Chance from '../SkaterPhotos/Fighting.png'
+import Daisy from '../SkaterPhotos/Daisy.jpg'
+import Dani from '../SkaterPhotos/Dani.png'
+import Dawna from '../SkaterPhotos/Dawna.png'
+import Ethyl from '../SkaterPhotos/Ethyl.png'
+import Femme from '../SkaterPhotos/Femme.png'
+import Ghost from '../SkaterPhotos/Ghost.jpg'
+import Habbitch from '../SkaterPhotos/Bad.jpg'
+import Hartlyn from '../SkaterPhotos/Hartlyn.png'
+import Hex from '../SkaterPhotos/Hex.png'
+import Rails from '../SkaterPhotos/Rails.jpg'
+import Salty from '../SkaterPhotos/Assault.jpg'
+import Scar from '../SkaterPhotos/Scarlet.jpg'
+import Slam from '../SkaterPhotos/Colonel.jpg'
+import Strange from '../SkaterPhotos/Strange.png'
+import Vanna from '../SkaterPhotos/Vanna.jpg'
 
 const allSkaters = [{
     skaterId: 1,
@@ -48,7 +62,7 @@ const allSkaters = [{
     photo: Beaux
 }, {
     skaterId: 7,
-    skaterName: 'Beetle Jukes',
+    skaterName: 'Beetlejukes',
     skaterNumber: '3 T1MES',
     team: 'Hired Gun$',
     photo: Bj
@@ -76,6 +90,96 @@ const allSkaters = [{
     skaterNumber: '1113',
     team: 'Hellcats',
     photo: Cyd
+}, {
+    skaterId: 12,
+    skaterName: 'Daisy Confused',
+    skaterNumber: 'Alright x3',
+    team: 'Holy Rollers',
+    photo: Daisy
+}, {
+    skaterId: 13,
+    skaterName: 'Dani Darko',
+    skaterNumber: '28064212',
+    team: 'Cherry Bombs',
+    photo: Dani
+}, {
+    skaterId: 14,
+    skaterName: 'Dawna Destruction',
+    skaterNumber: '2012',
+    team: 'Cherry Bombs',
+    photo: Dawna
+}, {
+    skaterId: 15,
+    skaterName: 'Ethyl Murder',
+    skaterNumber: '11-14',
+    team: 'Cherry Bombs',
+    photo: Ethyl
+}, {
+    skaterId: 16,
+    skaterName: 'Feared Al',
+    skaterNumber: '41',
+    team: 'Holy Rollers',
+    photo: Al
+}, {
+    skaterId: 17,
+    skaterName: 'Feared Al',
+    skaterNumber: '41',
+    team: 'Holy Rollers',
+    photo: Al
+}, {
+    skaterId: 18,
+    skaterName: `Femme O'Naide`,
+    skaterNumber: '48',
+    team: 'Holy Rollers',
+    photo: Femme
+}, {
+    skaterId: 19,
+    skaterName: 'Fighting Chance',
+    skaterNumber: '1-2 Punch',
+    team: 'Holy Rollers',
+    photo: Chance
+}, {
+    skaterId: 20,
+    skaterName: 'Ghost in the Chelle',
+    skaterNumber: '2nd GIG',
+    team: 'Holy Rollers',
+    photo: Ghost
+}, {
+    skaterId: 21,
+    skaterName: 'Hartlyn',
+    skaterNumber: '1022',
+    team: 'Holy Rollers',
+    photo: Hartlyn
+}, {
+    skaterId: 22,
+    skaterName: 'Hex Appeal',
+    skaterNumber: 'XXX',
+    team: 'Holy Rollers',
+    photo: Hex
+}, {
+    skaterId: 23,
+    skaterName: 'Rails from the Crypt',
+    skaterNumber: `86'd`,
+    team: 'Holy Rollers',
+    photo: Rails
+}, {
+    skaterId: 24,
+    skaterName: 'Scarlet Harlot',
+    skaterNumber: '#1 Crush',
+    team: 'Holy Rollers',
+    photo: Scar
+}, {
+    skaterId: 25,
+    skaterName: 'Stranger Thing',
+    skaterNumber: '1983',
+    team: 'Holy Rollers',
+    photo: Strange
+}, {
+    skaterId: 26,
+    skaterName: 'Vanna FOok',
+    skaterNumber: 'Y45',
+    team: 'Holy Rollers',
+    photo: Vanna
 }]
 
 export default allSkaters
